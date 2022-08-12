d6176f524d4bc89355cca13c0fa09dcfffd476f5
cristobal nyram <crisco.nyram@gmail.com>
2022-08-12 08:22:54 -0500

    mejoras en la bd

--- modules/conexion.php
+++ modules/conexion.php
@@ -1,7 +1,11 @@
 <?php
 date_default_timezone_set('America/Mexico_City');
+$servername = "https://school-system.dvtecam.com/";
+$database = "u445726646_db_escuela";
+$username = "u445726646_db_escuela";
+$password = "Rompecaras2381208087";
 
-$conexion = mysqli_connect("127.0.0.1", "root", "", "db_escuela","3309");
+$conexion = mysqli_connect($servername, $username, $password, $database);
 
 if (mysqli_connect_errno()) {
 	printf("Falló la conexión: %s\n", mysqli_connect_error());
